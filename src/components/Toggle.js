2abd3aff28254c0d7059863ddf01ea32d2f68f68
Dennis Musembi <dennismusembi344@gmail.com>
2023-04-23 03:53:13 +0300

    complete

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React,{useState} from "react";
+
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [switchh,setSwitch]=useState(false)
+  function Switch(){
+    setSwitch((switchh)=>!switchh)
+  }
+  const color=switchh?"red":"white"
+  
+  return <button style={{background:color}} onClick={Switch}>{switchh?"ON":"OFF"}</button>;
 }
 
 export default Toggle;
